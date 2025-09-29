8eab6505040391ba3d7cfaf58a19fabaeb216e31
emrah onat <eonat87@yahoo.com>
2025-09-30 00:19:05 +0200

    first commit

---
+++ main.m
@@ -0,0 +1,17 @@
+%% Interferogram Despekling Algorithms
+% 
+% 0 - Input Images (Interferograms)
+% 1 - Despekling Algorithms
+% 2 - Phase Unwrapping Algorithms
+%
+% Dr. Emrah Onat
+% 30.09.2025
+% 
+
+%
+
+%% Main Code 
+
+clear all
+close all
+clc
